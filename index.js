2cf93aaa8b4df8ef84df0e5eedb3a377ea9ac195
Amanov Dastan <dastanamanov007@gmail.com>
2021-08-28 21:08:35 +0500

    change css file

--- index.js
+++ index.js
@@ -3,43 +3,32 @@ let elInput = document.querySelector("#input-search")
 let list = document.querySelector(".list")
 let template = document.querySelector("#template").content;
 
-async function fetchdata(){
 
-    const response = await fetch(`https://pixabay.com/api/videos/?key=23037258-af0f917717571caabf69b3a7f`);
+elForm.addEventListener('submit', e =>{
+     e.preventDefault()
+     list.innerHTML = null;
+async function fetchdata(){
+    const response = await fetch(`https://pixabay.com/api/videos/?key=23037258-af0f917717571caabf69b3a7f&lang=ru&q=${elInput.value}`);
     const data = await response.json();
     console.log(data);
-    
     renderVideos = (dataApi)=>{
         dataApi.forEach(elem => {
             let {tags, videos:{tiny:{url}}} = elem
-
+            
             let cloneTemplate = template.cloneNode(true)
             cloneTemplate.querySelector("#add-data").setAttribute("src", url)
             cloneTemplate.querySelector(".video-tag").textContent = tags
-        
+            
+            if(elInput.value == null){
+                
+            }
+
             list.appendChild(cloneTemplate)
         });
     }
     renderVideos(data.hits)
 
-// renderTag = (dataTag, element)=>{
-//     elForm.addEventListener('submit', e =>{
-//         e.preventDefault()
-//         let regex =new RegExp(elInput.value.trim(), 'gi')
-//         let videoList = []
-//         dataTag.forEach(elem =>{
-//             elem.forEach( elem =>{
-//                 if(elem.tags.match(regex)){
-//                     videoList.push(elem.tags)
-//                 }
-//                 else{
-//                     elem
-//                 }
-//             })
-//         }) 
-//     })
-// }
-// renderTag(data, list)
-
 }
 fetchdata()
+
+})
