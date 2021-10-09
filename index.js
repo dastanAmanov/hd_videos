f0edcf2f2e85ec42764be67bd9b490b9c3c77ff2
dastanamanov <dastanamanov007@gmail.com>
2021-10-09 18:12:19 +0500

    add select element

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ let template = document.querySelector("#template").content;
 
 elForm.addEventListener('submit', e =>{
      e.preventDefault()
-     list.innerHTML = null;;
+     list.innerHTML = null;
+    let KEY = "23037258-af0f917717571caabf69b3a7f"
+
+
 async function fetchdata(){
-    const response = await fetch(`https://pixabay.com/api/videos/?key=23037258-af0f917717571caabf69b3a7f&lang=ru&q=${elInput.value}&page=1`);
+    const response = await fetch(`https://pixabay.com/api/videos/?key=${KEY}&lang=ru&q=${elInput.value}&category=${select.value}&page=1`); //<--- end point
     const data = await response.json();
-    console.log(data);
+
+
+    console.log(data); 
     renderVideos = (dataApi)=>{
         dataApi.forEach(elem => {
             let {tags, videos:{tiny:{url}}} = elem
