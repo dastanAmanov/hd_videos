5fc43e67b5817b88e02a2d4ad051b1ffff0154bb
dastanamanov <dastanamanov007@gmail.com>
2021-10-10 15:33:49 +0500

    add css media

--- index.js
+++ index.js
@@ -2,32 +2,62 @@ let elForm = document.querySelector("#form")
 let elInput = document.querySelector("#input-search")
 let list = document.querySelector(".list")
 let template = document.querySelector("#template").content;
+let btnPrev = document.querySelector('#btn-prev');
+let btnNext = document.querySelector('#btn-next');
 
-elForm.addEventListener('submit', e =>{
-     e.preventDefault()
-     list.innerHTML = null;
-    let KEY = "23037258-af0f917717571caabf69b3a7f"
+let KEY = "23037258-af0f917717571caabf69b3a7f"
+let pageCount = 1
 
+renderVideos = (dataApi)=>{
+    dataApi.forEach(elem => {
+        let {tags, videos:{tiny:{url}}} = elem
+
+        let cloneTemplate = template.cloneNode(true)
+        cloneTemplate.querySelector("#add-data").setAttribute("src", url)
+        cloneTemplate.querySelector(".video-tag").textContent = tags
+
+        list.appendChild(cloneTemplate)
+
+        if(pageCount <= 1){
+            btnPrev.disabled = true;
+        }else{
+            btnPrev.disabled = false;
+        }
+    })
+}
 
 async function fetchdata(){
-    const response = await fetch(`https://pixabay.com/api/videos/?key=${KEY}&lang=ru&q=${elInput.value}&category=${select.value}&page=1`); //<--- end point
+    list.innerHTML = null;
+    const response = await fetch(`https://pixabay.com/api/videos/?key=${KEY}&lang=ru&q=${elInput.value}&category=${select.value}&page=${pageCount}`); //<--- endpoint
     const data = await response.json();
-
-
-    console.log(data); 
-    renderVideos = (dataApi)=>{
-        dataApi.forEach(elem => {
-            let {tags, videos:{tiny:{url}}} = elem
-            
-            let cloneTemplate = template.cloneNode(true)
-            cloneTemplate.querySelector("#add-data").setAttribute("src", url)
-            cloneTemplate.querySelector(".video-tag").textContent = tags
-        
-            list.appendChild(cloneTemplate)
-        });
+    let total = Math.ceil(data.totalHits / 50)
+    
+    if(pageCount == total){
+        btnNext.disabled = true
+    }else{
+        btnNext.disabled = false
     }
+ 
     renderVideos(data.hits)
+    
 }
 fetchdata()
 
+
+btnPrev.addEventListener('click', ()=>{
+    pageCount--
+    console.log(pageCount);
+    fetchdata()
+})
+btnNext.addEventListener('click', ()=>{
+    pageCount++
+    console.log(pageCount);
+    fetchdata()
+})
+
+elForm.addEventListener('submit', e =>{
+    e.preventDefault()
+    
+    fetchdata()
+    
 })
